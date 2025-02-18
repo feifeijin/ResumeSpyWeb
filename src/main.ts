86424753ffb5b7b6006719c5c09def4a6d34954b
Feifei <dsf_jyf@qq.com>
2025-02-18 17:39:15 +0900

    init markdown editor and try to use it.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { registerMarkdownEditor } from './plugins/v-md-editor'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+registerMarkdownEditor(app)
 
 app.use(createPinia())
 app.use(router)
---
+++ src/types/v-md-editor.d.ts
@@ -0,0 +1,2 @@
+declare module '@kangc/v-md-editor/lib/codemirror-editor'
+declare module '@kangc/v-md-editor/lib/theme/github.js'
