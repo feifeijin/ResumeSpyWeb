630da618a5cbd9526c0552fc939b202fdbc9fbfb
Feifei <dsf_jyf@qq.com>
2025-02-20 15:40:53 +0900

    add banner to home page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import AppBar from './components/app/AppBar.vue'
 </script>
 
 <template>
-  <!-- <v-app>
+  <v-app>
     <app-bar />
-  </v-app> -->
-  <header>
+  </v-app>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -19,7 +20,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/editor">editor</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
 
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
