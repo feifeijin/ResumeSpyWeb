a783858ba0e27cb4761997a6ed20502319c786c9
Feifei <dsf_jyf@qq.com>
2025-03-09 16:38:12 +0900

    1, add tabs to createView page. 2, remove unused tests fot build.

--- src/plugins/v-md-editor.ts
+++ src/plugins/v-md-editor.ts
@@ -3,6 +3,7 @@ import VMdEditor from '@kangc/v-md-editor/lib/codemirror-editor'
 import '@kangc/v-md-editor/lib/style/codemirror-editor.css'
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js'
 import '@kangc/v-md-editor/lib/theme/style/github.css'
+import enUS from '@kangc/v-md-editor/lib/lang/en-US'
 
 // Resources for the codemirror editor
 import Codemirror from 'codemirror'
@@ -26,6 +27,7 @@ import 'codemirror/addon/scroll/simplescrollbars.css'
 // style
 import 'codemirror/lib/codemirror.css'
 
+VMdEditor.lang.use('en-US', enUS)
 VMdEditor.Codemirror = Codemirror
 VMdEditor.use(githubTheme)
 VMdEditor.Codemirror = Codemirror
--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,5 +1,26 @@
+<!-- filepath: /Users/ws/Documents/DotCore/ResumeSpyWeb/src/views/CreateView.vue -->
 <template>
-  <v-md-editor height="800px"></v-md-editor>
+  <v-container>
+    <v-tabs v-model="activeTab" background-color="primary" dark>
+      <v-tab v-for="tab in tabs" :key="tab">{{ tab }}</v-tab>
+    </v-tabs>
+
+    <v-tabs-window v-model="activeTab">
+      <v-tabs-window-item v-for="(tab, index) in tabs" :key="tab">
+        <v-md-editor v-model="editors[index]" height="800px">{{ tab }}</v-md-editor>
+      </v-tabs-window-item>
+    </v-tabs-window>
+    <!-- <v-tabs-items v-model="activeTab">
+      <v-tab-item v-for="(tab, index) in tabs" :key="tab">
+      </v-tab-item>
+    </v-tabs-items> -->
+  </v-container>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const tabs = ['ENG', 'JPN', 'CN']
+const activeTab = ref(0)
+const editors = ['ENG', 'JPN', 'CN'] // Initialize an array with empty strings for each tab
+</script>
